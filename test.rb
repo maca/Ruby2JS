be047d12cc9ee9fc1f346033a955ef99a020ef21
Macario <macarui@gmail.com>
2009-06-11 19:41:48 -0500

    deleted some files

--- test.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'pp'
-require 'ruby_parser'
-
-
-
-
-"1 + (1 + (1 + (1 * (2 - 1))))"
-s(:call, s(:lit, 1), :+, s(:arglist, s(:call, s(:lit, 1), :+, s(:arglist, s(:call, s(:lit, 1), :+, s(:arglist, s(:call, s(:lit, 1), :*, s(:arglist, s(:call, s(:lit, 2), :-, s(:arglist, s(:lit, 1)))))))))))
