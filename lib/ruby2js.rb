9f7f45d790a0132e5304ab8c0e78dd5c707ff10f
Macario <macarui@gmail.com>
2009-06-11 19:47:12 -0500

    bumped gem version in order to rebuild

--- lib/ruby2js.rb
+++ lib/ruby2js.rb
@@ -3,7 +3,7 @@ require 'sexp_processor'
 # $:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 class Ruby2JS
-  VERSION   = '0.0.1'
+  VERSION   = '0.0.2'
   LOGICAL   = :and, :not, :or
   OPERATORS = [:[], :[]=], [:not], [:*, :/, :%], [:+, :-, :<<], [:and], [:or]
   
